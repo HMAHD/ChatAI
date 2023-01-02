8d04111b138ac55a96f4a6b9a4e633c2cd4bf844
Akash Hasendra <akashhasendra2@gmail.com>
2023-01-03 00:28:01 +0530

    first commit

---
+++ client/script.js
@@ -0,0 +1,122 @@
+//AI & User assets
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+// loading the chat 
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '🔸';
+
+    if (element.textContent === '🔸🔸🔸🔸') {
+      element.textContent = '';
+    }
+  }, 300);
+}
+
+//Function to show as typing
+function typeText(element, text){
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text.charAt(index);
+      index++;
+    }else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+//unique id for all message for map
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe (isAi, value, uniqueId){
+  return (
+    `
+    <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+            <div class="profile">
+                <img 
+                  src=${isAi ? bot : user} 
+                  alt="${isAi ? 'bot' : 'user'}" 
+                />
+            </div>
+            <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+    </div>
+`
+  )
+}
+
+// chat container adjustments
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  //user's chatstripe 
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+ 
+  // to clear the textarea input 
+  form.reset()
+
+  //Ai chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  // loader
+  loader(messageDiv);
+
+  //fetch data from server - openai response
+  const response = await fetch('http://localhost:5000',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  //stop loading and reload
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+
+  if (response.ok){
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText = (messageDiv, parsedData);
+  }else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong Contact me -https://github.com/HMAHD";
+    alert(err);
+  }
+
+  }
+
+//Add Enter key functionality
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) =>{
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+})
