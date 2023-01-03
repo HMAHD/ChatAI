f008e66fbf7b16ee468cf83d539df49b078aa25c
Akash Hasendra <akashhasendra2@gmail.com>
2023-01-03 18:31:08 +0530

    Changes server

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server - openai response
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://chatai-rmfs.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
